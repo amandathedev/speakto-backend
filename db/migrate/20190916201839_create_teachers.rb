aa7ade800dd09412552ff542bbc82f894a4eedf1
Amanda Treutler <amandathedev@gmail.com>
2019-09-16 19:05:59 -0500

    corrects schema

--- db/migrate/20190916201839_create_teachers.rb
+++ db/migrate/20190916201839_create_teachers.rb
@@ -6,10 +6,8 @@ class CreateTeachers < ActiveRecord::Migration[5.2]
       t.string :email
       t.string :password
       t.string :skype_id
-      t.string :resume_url
       t.string :photo_url
       t.string :intro_text
-      t.integer :average_rating
       t.integer :volunteer_points
       t.integer :lessons_completed
       t.integer :income_balance
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,123 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Teacher.create(
+  name: "Pam Beasley",
+  username: "spamster",
+  email: "spamster@email.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
+
+Teacher.create(
+  name: "Jim Halpert",
+  username: "bigtuna55",
+  email: "jamesh@email.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
+
+Teacher.create(
+  name: "Dwight Schrute",
+  username: "beetfarmer12",
+  email: "schrutefarms@email.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
+
+Teacher.create(
+  name: "Michael Scott",
+  username: "littlekidlvr",
+  email: "michaelgscott@email.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
+
+Teacher.create(
+  name: "Andrew Bernard",
+  username: "narddog",
+  email: "cornellandy@email.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
+
+Teacher.create(
+  name: "Phyllis Vance",
+  username: "fridgewife",
+  email: "pvance@vancerefrigeration.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
+
+Teacher.create(
+  name: "Angela Martin",
+  username: "littledrummergirl",
+  email: "amartin@email.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
+
+Teacher.create(
+  name: "Kevin Malone",
+  username: "scrantonicity",
+  email: "kevinpostmalone@email.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
+
+Teacher.create(
+  name: "Pam Beasley",
+  username: "spamster",
+  email: "spamster@email.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
+
+Teacher.create(
+  name: "Pam Beasley",
+  username: "spamster",
+  email: "spamster@email.com",
+  password: ,
+  skype_id: ,
+  resume_url: ,
+  photo_url: ,
+  intro_text: ,
+  average_rating: 
+)
