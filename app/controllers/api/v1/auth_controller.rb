6715840a5f587cddbf46db902188befc181b8f24
Amanda Treutler <amandathedev@gmail.com>
2019-09-22 21:08:12 -0500

    fixes auth controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,28 +1,31 @@
 class Api::V1::AuthController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  # skip_before_action :authorized, only: [:create]
 
   def create #POST /api/v1/login
-    @teacher = Teacher.find_by(username: teacher_login_params[:username])
+    # @teacher = Teacher.find_by(username: teacher_login_params[:username])
     @student = Student.find_by(username: student_login_params[:username])
+    p params
     
-    if @teacher && @teacher.authenticate(teacher_login_params[:password_digest])
-      @token = encode_token({ teacher_id: @teacher.id })
-      render json: { teacher: TeacherSerializer.new(@teacher), jwt: @token }, status: :accepted
-    elsif
-      @student && @student.authenticate(student_login_params[:password_digest])
+    # if 
+    #   @teacher && @teacher.authenticate(teacher_login_params[:password_digest])
+    #   @token = encode_token({ teacher_id: @teacher.id })
+    #   render json: { teacher: TeacherSerializer.new(@teacher), jwt: @token }, status: :accepted
+    # elsif
+      @student && @student.authenticate(student_login_params[:password])
       @token = encode_token({ student_id: @student.id })
-      render json: {student: StudentSerializer.new(@student), jwt: @token }, status: accepted
-    else
-      render json: { message: 'Invalid username or password' }, status: :unauthorized
-    end
+      render json: {student: StudentSerializer.new(@student), jwt: @token }, status: :accepted
+    # else
+    #   render json: { message: 'Invalid username or password' }, status: :unauthorized
+  # end  
+  end
 
     private
 
     def teacher_login_params
-      params.require(:teacher).permit(:username, :password_digest)
+      params.require(:teacher).permit(:username, :password)
     end
 
     def student_login_params
-      params.require(:student).permit(:username, :password_digest)
+      params.require(:student).permit(:username, :password)
     end
 end
