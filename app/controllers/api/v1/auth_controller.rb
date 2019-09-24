7d41a55ca69f0a2b43dbea11df110b24010c483d
Amanda Treutler <amandathedev@gmail.com>
2019-09-23 19:28:11 -0500

    routing'

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::AuthController < ApplicationController
   # skip_before_action :authorized, only: [:create]
 
   def create #POST /api/v1/login
-    @student = Student.find_by(username: student_login_params[:username])
-    @teacher = Teacher.find_by(username: teacher_login_params[:username])
+    @student = Student.find_by(username: user_login_params[:username])
+    @teacher = Teacher.find_by(username: user_login_params[:username])
     @user = nil
     @identity = nil
 
@@ -16,11 +16,11 @@ class Api::V1::AuthController < ApplicationController
     end
       
       if @identity === "student"
-        @student && @student.authenticate(student_login_params[:password])
+        @student && @student.authenticate(user_login_params[:password])
         @token = encode_token({ user_id: @student.id, identity: @identity })
         render json: {user: StudentSerializer.new(@student), jwt: @token }, status: :accepted
       elsif @identity === "teacher"
-        @teacher && @teacher.authenticate(teacher_login_params[:password])
+        @teacher && @teacher.authenticate(user_login_params[:password])
         @token = encode_token({ user_id: @teacher.id, identity: @identity })
         render json: {user: TeacherSerializer.new(@teacher), jwt: @token }, status: :accepted
       else 
@@ -30,11 +30,7 @@ class Api::V1::AuthController < ApplicationController
 
     private
 
-    def teacher_login_params
-      params.require(:teacher).permit(:username, :password)
-    end
-
-    def student_login_params
-      params.require(:student).permit(:username, :password)
+    def user_login_params 
+      params.require(:user).permit(:username, :password)
     end
 end
