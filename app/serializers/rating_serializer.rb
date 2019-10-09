4690b43726232b395cdd4b67a74dbcbe4dd3a825
Amanda Treutler <amandathedev@gmail.com>
2019-10-09 09:16:31 -0500

    fix

--- app/serializers/rating_serializer.rb
+++ app/serializers/rating_serializer.rb
@@ -1,4 +1,4 @@
 class RatingSerializer < ApplicationSerializer
-  belongs_to :lesson, class_name: "Lesson"
+  # belongs_to :lesson, class_name: "Lesson"
   attributes :id, :rating, :comment, :lesson_id
 end
